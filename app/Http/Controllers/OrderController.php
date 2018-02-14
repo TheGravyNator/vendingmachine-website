eeba6ceecc87111c5c666d0f620a0f4ae142a174
“Christian <christian@23g.nl>
2018-02-14 15:47:20 +0100

    Added Out Of Stock response

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -45,59 +45,66 @@ class OrderController extends Controller
       {
         return redirect('order')->with('status', 'Er is iets fout gegaan.');
       }
-      $order = new Order;
-      $prices = [
-        'slot_1' => Slot::where('slot_id', 1)->first(),
-        'slot_2' => Slot::where('slot_id', 2)->first(),
-        'slot_3' => Slot::where('slot_id', 3)->first(),
-        'slot_4' => Slot::where('slot_id', 4)->first(),
-        'slot_5' => Slot::where('slot_id', 5)->first(),
-      ];
-      if(request('slot') == 'slot_1')
+      if($response->getBody() == "Out of stock")
       {
-        $soda_type = Slot::where('slot_id', 1)->value('name_soda');
-        $order->user_id = Auth::user()->id;
-        $order->soda_type = $soda_type;
-        $order->amount = request('soda_amount');
-        $order->price = Slot::where('slot_id', 1)->value('price') * request('soda_amount');
-        $order->save();
+        return redirect('order')->with('status', 'Deze frisdrank is niet meer aanwezig in het frisdrankautomaat.');
       }
-      if(request('slot') == 'slot_2')
+      else
       {
-        $soda_type = Slot::where('slot_id', 2)->value('name_soda');
-        $order->user_id = Auth::user()->id;
-        $order->soda_type = $soda_type;
-        $order->amount = request('soda_amount');
-        $order->price = Slot::where('slot_id', 2)->value('price') * request('soda_amount');
-        $order->save();
+        $order = new Order;
+        $prices = [
+          'slot_1' => Slot::where('slot_id', 1)->first(),
+          'slot_2' => Slot::where('slot_id', 2)->first(),
+          'slot_3' => Slot::where('slot_id', 3)->first(),
+          'slot_4' => Slot::where('slot_id', 4)->first(),
+          'slot_5' => Slot::where('slot_id', 5)->first(),
+        ];
+        if(request('slot') == 'slot_1')
+        {
+          $soda_type = Slot::where('slot_id', 1)->value('name_soda');
+          $order->user_id = Auth::user()->id;
+          $order->soda_type = $soda_type;
+          $order->amount = request('soda_amount');
+          $order->price = Slot::where('slot_id', 1)->value('price') * request('soda_amount');
+          $order->save();
+        }
+        if(request('slot') == 'slot_2')
+        {
+          $soda_type = Slot::where('slot_id', 2)->value('name_soda');
+          $order->user_id = Auth::user()->id;
+          $order->soda_type = $soda_type;
+          $order->amount = request('soda_amount');
+          $order->price = Slot::where('slot_id', 2)->value('price') * request('soda_amount');
+          $order->save();
+        }
+        if(request('slot') == 'slot_3')
+        {
+          $soda_type = Slot::where('slot_id', 3)->value('name_soda');
+          $order->user_id = Auth::user()->id;
+          $order->soda_type = $soda_type;
+          $order->amount = request('soda_amount');
+          $order->price = Slot::where('slot_id', 3)->value('price') * request('soda_amount');
+          $order->save();
+        }
+        if(request('slot') == 'slot_4')
+        {
+          $soda_type = Slot::where('slot_id', 4)->value('name_soda');
+          $order->user_id = Auth::user()->id;
+          $order->soda_type = $soda_type;
+          $order->amount = request('soda_amount');
+          $order->price = Slot::where('slot_id', 4)->value('price') * request('soda_amount');
+          $order->save();
+        }
+        if(request('slot') == 'slot_5')
+        {
+          $soda_type = Slot::where('slot_id', 5)->value('name_soda');
+          $order->user_id = Auth::user()->id;
+          $order->soda_type = $soda_type;
+          $order->amount = request('soda_amount');
+          $order->price = Slot::where('slot_id', 5)->value('price') * request('soda_amount');
+          $order->save();
+        }
+        return redirect('/order');
       }
-      if(request('slot') == 'slot_3')
-      {
-        $soda_type = Slot::where('slot_id', 3)->value('name_soda');
-        $order->user_id = Auth::user()->id;
-        $order->soda_type = $soda_type;
-        $order->amount = request('soda_amount');
-        $order->price = Slot::where('slot_id', 3)->value('price') * request('soda_amount');
-        $order->save();
-      }
-      if(request('slot') == 'slot_4')
-      {
-        $soda_type = Slot::where('slot_id', 4)->value('name_soda');
-        $order->user_id = Auth::user()->id;
-        $order->soda_type = $soda_type;
-        $order->amount = request('soda_amount');
-        $order->price = Slot::where('slot_id', 4)->value('price') * request('soda_amount');
-        $order->save();
-      }
-      if(request('slot') == 'slot_5')
-      {
-        $soda_type = Slot::where('slot_id', 5)->value('name_soda');
-        $order->user_id = Auth::user()->id;
-        $order->soda_type = $soda_type;
-        $order->amount = request('soda_amount');
-        $order->price = Slot::where('slot_id', 5)->value('price') * request('soda_amount');
-        $order->save();
-      }
-      return redirect('/order');
     }
 }
